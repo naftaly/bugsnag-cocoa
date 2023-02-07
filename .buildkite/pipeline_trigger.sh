797235ee96e4a6dd9f89a44381cf4c48ba4c45a4
Steve Kirkland-Walton <steve@bugsnag.com>
2023-02-07 22:15:16 +0000

    Do not add block step if not testing on BS

--- .buildkite/pipeline_trigger.sh
+++ .buildkite/pipeline_trigger.sh
@@ -6,9 +6,11 @@ if [[ "$BUILDKITE_MESSAGE" == *"[full ci]"* ||
   echo "Running full build"
   buildkite-agent pipeline upload .buildkite/pipeline.full.yml
 else
-  # Basic build, but allow a full build to be triggered
-  echo "Running basic build"
-  buildkite-agent pipeline upload .buildkite/block.full.yml
+  if [[ "$BUILDKITE_MESSAGE" != *"[nobs]"* &&
+        "$DEVICE_FARM" != *"NO_BS"* ]]; then
+    # Basic build, but allow a full build to be triggered
+    buildkite-agent pipeline upload .buildkite/block.full.yml
+  fi
 fi
 
 # Run BrowserStack steps unless instructed not to
